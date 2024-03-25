edd4464fd0709fbfd35e4fbf21e6fa373023ee52
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-03-25 21:17:19 +0100

    add middlewares, json responses and express routes

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express')
+const morgan = require('morgan')
+const { success } = require('./helper.js')
+let pokemons = require('./db/mock-pokemon');
 
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => res.send('Hello, World ! ✋'))
+app.use(morgan('dev'))
+
+// app.use((req, res, next) => {
+//     console.log(`URL : ${req.url}`)
+//     next()
+// })
+
+
+app.get('/', (req,res) => res.send('Hello, Express ! ✋'))
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    const message = 'Un pokémoan a bien été trouvé.'
+    res.json(success(message, pokemon))
+})
+
+app.get('/api/pokemons', (req, res) => {
+    const message = 'La liste des pokémons a bien été récupérée.'
+    res.json(success(message, pokemons))
+})
+
+
 
 app.listen(port, () => console.log(`My node app is running on : http://localhost:${port}`))
